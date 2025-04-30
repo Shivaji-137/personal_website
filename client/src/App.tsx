f0314900d2938a55ad492739ec6dc13f968d0476
Shivaji-137 <shivajichaulagain@gmail.com>
2025-04-30 22:22:17 +0545

    removed hashrouter from app.tsx and no changes in main.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
@@ -20,18 +20,16 @@ function App() {
       <ThemeProvider defaultTheme="dark">
         <TooltipProvider>
           <Toaster />
-          <Router>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/projects" element={<ProjectsPage />} />
-              <Route path="/research" element={<ResearchPage />} />
-              <Route path="/blog" element={<BlogPage />} />
-              <Route path="/blogpost/:id" element={<BlogPostPage />} />
-              <Route path="/cv" element={<CVPage />} />
-              <Route path="/contact" element={<ContactPage />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/research" element={<ResearchPage />} />
+            <Route path="/blog" element={<BlogPage />} />
+            <Route path="/blogpost/:id" element={<BlogPostPage />} />
+            <Route path="/cv" element={<CVPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </TooltipProvider>
       </ThemeProvider>
     </QueryClientProvider>
