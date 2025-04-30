0b8ac5ce696d81bb5d0cd024f34114c9f90832e8
Shivaji-137 <shivajichaulagain@gmail.com>
2025-04-30 07:02:11 +0545

    updated app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/ui/theme-provider";
-import { Routes, Route } from "react-router-dom";
+
 import HomePage from "@/pages/HomePage";
 import ProjectsPage from "@/pages/ProjectsPage";
 import ResearchPage from "@/pages/ResearchPage";
 import BlogPage from "@/pages/BlogPage";
-import BlogPostPage from "@/blogpost/BlogPostPage";
+import BlogPostPage from "@/blogpost/BlogPostPage"; // 👈 Add this
 import CVPage from "@/pages/CVPage";
 import ContactPage from "@/pages/ContactPage";
 import NotFound from "@/pages/not-found";
-
-function AppRoutes() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/projects" element={<ProjectsPage />} />
-      <Route path="/research" element={<ResearchPage />} />
-      <Route path="/blog" element={<BlogPage />} />
-      <Route path="/blogpost/:id" element={<BlogPostPage />} /> 
-      <Route path="/cv" element={<CVPage />} />
-      <Route path="/contact" element={<ContactPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-}
 
 function App() {
   return (
@@ -34,7 +20,18 @@ function App() {
       <ThemeProvider defaultTheme="dark">
         <TooltipProvider>
           <Toaster />
-          <AppRoutes />
+          <Router>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/projects" element={<ProjectsPage />} />
+              <Route path="/research" element={<ResearchPage />} />
+              <Route path="/blog" element={<BlogPage />} />
+              <Route path="/blogpost/:id" element={<BlogPostPage />} /> {/* ✅ */}
+              <Route path="/cv" element={<CVPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="*" element={<NotFound />} /> {/* 404 fallback */}
+            </Routes>
+          </Router>
         </TooltipProvider>
       </ThemeProvider>
     </QueryClientProvider>
