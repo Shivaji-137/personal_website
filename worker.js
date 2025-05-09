51499428c3d6416f2e23ce913ad2c7f0692d9acd
Shivaji-137 <shivajichaulagain@gmail.com>
2025-05-09 11:17:46 +0545

    fixes database binding to contact

--- worker.js
+++ worker.js
@@ -1,21 +1,18 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+export default {
+  async fetch(request, env, ctx) {
+    if (request.method === 'POST') {
+      const data = await request.json();
+      const { name, email, subject, message } = data;
 
-async function handleRequest(request) {
-  if (request.method === 'POST') {
-    const data = await request.json();
-    
-    // Insert the data into the D1 database
-    const { name, email, subject, message } = data;
-    const query = `INSERT INTO contact_information (name, email, subject, message) VALUES (?, ?, ?, ?)`;
-    
-    try {
-      await contactform.prepare(query).bind(name, email, subject, message).run();
-      return new Response('Message stored successfully!', { status: 200 });
-    } catch (error) {
-      return new Response('Failed to store message.', { status: 500 });
+      const query = `INSERT INTO contact_information (name, email, subject, message) VALUES (?, ?, ?, ?)`;
+      try {
+        await env.contactform.prepare(query).bind(name, email, subject, message).run();
+        return new Response('Message stored successfully!', { status: 200 });
+      } catch (error) {
+        return new Response('Failed to store message.', { status: 500 });
+      }
     }
-  }
-  return new Response('Method not allowed', { status: 405 });
-} 
+
+    return new Response('Method Not Allowed', { status: 405 });
+  },
+};
