2fb4567882543eba8f267a76d9741d7eada3d68c
Shivaji-137 <shivajichaulagain@gmail.com>
2025-07-07 07:15:13 +0545

    added new project

--- client/src/sections/HeroSection.tsx
+++ client/src/sections/HeroSection.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 const NoticeBanner = () => {
   const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
   const messages = [
-    '🚀 New Project!!: Convolutional Neural Networks (CNNs) - Educational App'
+    '🚀 New Project!!: Nepal Foreign Trade Dashboard'
   ];
 
   const bannerRef = useRef<HTMLDivElement>(null);
@@ -106,7 +106,7 @@ const NoticeBanner = () => {
         {messages.map((message, index) => (
           <span key={index} className="mr-8">
             🚀{' '}
-            <a href='https://shivaji-interactiveguide-to-cnn.streamlit.app/' target="_blank" className="hover:text-pink-400 no-underline">
+            <a href='https://nepal-foreign-trade-dashboard.streamlit.app/' target="_blank" className="hover:text-pink-400 no-underline">
               {message}
             </a>
           </span>
