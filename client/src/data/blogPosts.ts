265eae0f86ab6b89d155db27775148fbd0752d1c
Shivaji-137 <shivajichaulagain@gmail.com>
2025-07-07 08:22:29 +0545

    added new blog post

--- client/src/data/blogPosts.ts
+++ client/src/data/blogPosts.ts
@@ -13,6 +13,17 @@ export interface BlogPost {
 
 export const blogPosts: BlogPost[] = [
 {
+    id: 4,
+    title: "Nepal Trade Dashboard: Making Trade Data Easy to Understand",
+    excerpt: "",
+    date: "July 7, 2025",
+    readTime: "7 min read",
+    category: ["Programming"],
+    tags: ["Python", "Nepal Foreign Trade", "Dashboard", "Data Visualization"],
+    markdownPath: "/markdown/4.md", // ✅ relative to public folder
+    image: "/blog_images/trade_background.png"
+  },
+{
     id: 1,
     title: "From Fear to Focus: My Transformative Journey at the Future Front Educators Leadership Lab",
     excerpt: "",
@@ -22,7 +33,6 @@ export const blogPosts: BlogPost[] = [
     tags: ["Leadership", "Effective communication", "Teaching Training", "Educators"],
     markdownPath: "/markdown/1.md", // ✅ relative to public folder
     image: "/blog_images/training.jpg"
-
   },
   {
     id: 2,
